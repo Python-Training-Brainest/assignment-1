3429219643abac26edff67d99377df462dca1689
Pablo Varela <2119094+pablojvarela@users.noreply.github.com>
2023-04-13 23:53:40 -0300

    Pablo's Hangman implementation.

--- hangman.py
+++ hangman.py
@@ -21,3 +21,52 @@ Word: j a _ a
 Guess a letter: v
 You guessed the word java !
 '''
+import sys
+
+
+def hangman(word):
+    tries = 6
+    guessed = False
+    used_letters = []
+    matched_letters = []
+
+    while tries > 0: 
+
+        print("\n")
+        print(f"You have {tries} tries left.")
+        print(f"Used letters: {' '.join(used_letters)}")
+        print("Word: ", " ".join([("_" if l not in matched_letters else l) for l in word]))
+        print("\n")
+
+        try:
+            guess = str(input("Guess a letter: "))
+
+            if len(guess) > 1: raise ValueError
+
+            elif guess in used_letters:
+                print("You already guessed this letter. Choose a different one.")
+
+            elif guess in word:
+                matched_letters.append(guess)
+                used_letters.append(guess)
+
+            else:
+                print(f"The letter '{guess}' is not in the word")
+                used_letters.append(guess)
+                tries -= 1
+
+                if tries == 0:
+                    print("You loose. :(")
+                    break
+
+            if all(letter in matched_letters for letter in word):
+                print(f"You guessed the word '{word}'.\n")
+                break
+
+        except ValueError:
+            print("You can only guess a single letter [a-z]")
+
+
+if __name__ == "__main__":
+    hangman(word=sys.argv[1])
+
